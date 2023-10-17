d9ebcd3f996ce1fae4c211100c9a1907c9c4e6bb
DAMMOU <udammou@gmail.com>
2023-10-17 16:02:08 +0200

    first commit

---
+++ src/main/java/com/lmnader/rhassociation/RhAssociationApplication.java
@@ -0,0 +1,13 @@
+package com.lmnader.rhassociation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhAssociationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhAssociationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lmnader/rhassociation/RhAssociationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmnader.rhassociation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhAssociationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
