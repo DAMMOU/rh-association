4dc3cac39db29be005d5b43d0261968e6c56f0c5
DAMMOU <udammou@gmail.com>
2023-10-18 12:25:29 +0200

    Add and test the CustomProperties class

--- src/main/java/com/lmnader/rhassociation/RhAssociationApplication.java
+++ src/main/java/com/lmnader/rhassociation/RhAssociationApplication.java
@@ -1,13 +1,21 @@
 package com.lmnader.rhassociation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RhAssociationApplication {
+public class RhAssociationApplication implements CommandLineRunner {
 
+	@Autowired
+	private CustomProperties customProperties;
 	public static void main(String[] args) {
 		SpringApplication.run(RhAssociationApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(customProperties.getApiUrl());
+	}
 }
